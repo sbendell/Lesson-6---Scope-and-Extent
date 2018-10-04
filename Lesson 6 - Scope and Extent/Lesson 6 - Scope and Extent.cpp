b5a77413781c5430091ebdd815f33b0968671787
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-04 14:07:20 +0100

    Copied code

--- Lesson 6 - Scope and Extent/Lesson 6 - Scope and Extent.cpp
+++ Lesson 6 - Scope and Extent/Lesson 6 - Scope and Extent.cpp
@@ -2,10 +2,45 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
-int main()
-{
-    return 0;
+void some_function() {
+	int a = 400;
+	char letter = 'b';
 }
 
+int global_variable = 50;
+
+int main() {
+
+	cout << "global variable contains the value " << global_variable << "\n";
+
+	int a = 100;
+	int* b;
+
+	{
+
+		int a = 200;
+
+		cout << "the value in a is " << a << "\n";
+
+		int* local_ptr = new int(300);
+
+		b = local_ptr;
+
+	}
+
+	cout << "the value in a is " << a << "\n";
+
+	cout << "the value pointed to by 'b' is " << *b << "\n";
+	delete b;
+	b = NULL;
+
+	std::cout << "output some data" << "\n";
+
+	some_function();
+
+	return 0;
+}
